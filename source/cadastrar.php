c9efe27a4ba51a2d69cc2cc4fcffd76d2d07eb0f
JardelAnton <jardellanton@gmail.com>
2017-09-25 11:46:05 -0300

    Formulário de cadastro de aluno.

---
+++ source/cadastrar.php
@@ -0,0 +1,21 @@
+<?php
+	include "funcoes.php";
+	session_start();
+	uffs_conexao_bd();
+	if(isset($_POST["cadastrar"])){
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		$first_name = $_POST["first_name"];
+		$last_name = $_POST["last_name"];
+		$enrollment = $_POST["enrollment"];
+		$email = $_POST["email"];
+		$registry_date = date("Y-m-d");
+		
+			$insere = "INSERT INTO `users`(`username`, `password`, `first_name`,`last_name`, `enrollment`, `email`, `registry_date`, `ban_status`, `user_role_id`) 
+			VALUES ('$username', '$password', '$first_name','$last_name', '$enrollment', '$email', '$registry_date', 0, 0) ";
+			mysqli_query($link,$insere);// or die("Nao inserido."); 
+		
+		
+		
+	}
+?>
