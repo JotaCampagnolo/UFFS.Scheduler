b36c312edf4071bac3b700ca7ce4aaa1c37f4c54
João M. Campagnolo <jota.campagnolo@gmail.com>
2017-09-26 08:56:49 -0300

    Função de Conexão com o BD.

--- source/funcoes.php
+++ source/funcoes.php
@@ -1,21 +1,24 @@
 <?php
-	function uffs_conexao_bd() {
-		$servidor = 'localhost';
-		$banco = 'mydb'; //nome do banco
-		$usuario = 'root'; //usuario
-		$senha = '123'; //senha
-		$link = mysqli_conect($servidor, $usuario, $senha) or die(mysql_error());
-		$db = mysqli_select_db($banco,$link) or die(mysql_error());
-		/*
-		if(!$link) {
-			echo "erro ao conectar ao banco de dados!";
-			exit();
-		}
-		*/
+function DBConection(){
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$database = "mydb";
+
+	$link = mysqli_connect($host, $user, $pass, $database);
+
+	if (!$link) {
+			echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+			exit;
 	}
-	
+
+	return $link;
+}
+
 	function uffs_login_user() {
-		
-		
+
+
 	}
 ?>
